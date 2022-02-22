e31020f45b40b1599ba0c86f9dacd7f9752b0cec
Abhijeet Pathania <94929386+Prkboltipublic257@users.noreply.github.com>
2022-02-23 02:58:40 +0530

    Update alive.py

--- plugins/alive.py
+++ plugins/alive.py
@@ -43,7 +43,7 @@ async def start_(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "➕ ❰ repo ❱ ➕", url=f"https://github.com/VENOMxCRAZY9/VENOMxOPxMUSIC"),
+                        "➕ ❰ repo ❱ ➕", url=f"https://github.com/Prkboltipublic257/LUCY-MUSIC"),
                         InlineKeyboardButton("• Cʜᴀɴɴᴇʟ", url=f"https://t.me/Statusforyoubby"),
                       InlineKeyboardButton("• Group", url=f"https://t.me/pagal_pantii"),
                   ],[
